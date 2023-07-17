5a494c1b968b245158885a08318866217e952036
Md-MahaburRahmanNadim <mdnadimhaaan120@gmail.com>
2023-07-17 15:28:59 +0100

    some notes on overall this 10. while loop

--- wp-content/themes/nadim-university/single.php
+++ wp-content/themes/nadim-university/single.php
@@ -5,8 +5,10 @@ while(have_posts()){
   the_post();?> 
   <h2><?php the_title() ?> </h2>
   <?php the_content()?> 
-  
+
 
 
 <?php
 }
+
+// let's open a page and see the result. we saw that this page also porwer by the index.php file code. that's why the title has a link. but we don't want that. we want to show the title without the link. how can we do that? for that we have to create a new file called page.php in the root directory of our theme. and copy the code from index.php and paste it in the page.php file. and remove the href from the title. and we are done.
