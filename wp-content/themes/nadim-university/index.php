9eb39e274cacff05fa2c21e6bd973162830e22af
Md-MahaburRahmanNadim <mdnadimhaaan120@gmail.com>
2023-07-17 13:44:08 +0100

    displiying post titile and discription

--- wp-content/themes/nadim-university/index.php
+++ wp-content/themes/nadim-university/index.php
@@ -1,1 +1,10 @@
-<h1>this is form nadim university</h1>
+<!-- lets say we want to show all the blog post in the index page of our webstie. how can we do that? -->
+
+<?php
+while(have_posts()){// 
+  the_post(); ?> <!--out the php mode and write html code while we are in the loop -->
+  <h2><a href="<?php the_permalink() ?>"><?php the_title() ?> </a></h2>
+  <?php the_content()?>
+  <hr>
+<?php // back to php mode
+};
