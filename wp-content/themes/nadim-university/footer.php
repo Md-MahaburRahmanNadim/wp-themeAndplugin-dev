921c3621f2a4b3210057b39e27eb1d59417f4118
Md-MahaburRahmanNadim <mdnadimhaaan120@gmail.com>
2023-07-17 17:14:02 +0100

    header & footer loading done next i will link css file in header

--- wp-content/themes/nadim-university/footer.php
+++ wp-content/themes/nadim-university/footer.php
@@ -1,1 +1,3 @@
-<h1>I am from footer</h1>
+<?php wp_footer() // this will inject all js file and also the top balck admin bar?>
+</body>
+</html>
--- wp-content/themes/nadim-university/header.php
+++ wp-content/themes/nadim-university/header.php
@@ -1,1 +1,10 @@
-<h1>I am form header</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <?php wp_head(); ?> <!-- this function pull out the all of the content related to the heaer --> 
+</head>
+<body>
+<!--   
+</body>
+</html> -->
+<!-- we have to close it in the footer.php file  -->
