a1e09fca26d1f28259878edb6ead38ba36e5a41c
Md-MahaburRahmanNadim <mdnadimhaaan120@gmail.com>
2023-07-19 07:55:46 +0100

    learning about parent & child page relationship next we will empade the side bar from where we will see that if a page has a child or not

--- wp-content/themes/nadim-university/page.php
+++ wp-content/themes/nadim-university/page.php
@@ -11,21 +11,39 @@
         </div>
       </div>
     </div>
+<!-- this is the parent child relationship div -->
+<!-- 
+hi github co lab 
+please explain get_the_ID() and wp_get_post_parent_id()
 
-    <div class="container container--narrow page-section">
+
+ -->
+
+  <div class="container container--narrow page-section">
+    <?php 
+    $parentId = wp_get_post_parent_id(get_the_ID());
+    
+    if($parentId){?>
       <div class="metabox metabox--position-up metabox--with-home-link">
         <p>
-          <a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span>
+          <a class="metabox__blog-home-link" href="<?php echo get_the_permalink($parentId)?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($parentId)?></a> <span class="metabox__main"><?php the_title()?></span>
         </p>
       </div>
 
-      <!-- <div class="page-links">
+  <?php
+  
+} ?>
+<!-- <?php echo wp_get_post_parent_id();
+echo get_the_ID(); // test code
+?>  -->
+
+      <div class="page-links">
         <h2 class="page-links__title"><a href="#">About Us</a></h2>
         <ul class="min-list">
           <li class="current_page_item"><a href="#">Our History</a></li>
           <li><a href="#">Our Goals</a></li>
         </ul>
-      </div> -->
+      </div>
 
       <div class="generic-content">
        <?php the_content() ?>
