2452ba05a4ff7d19cf82ef560e632c732f19e73a
Md-MahaburRahmanNadim <mdnadimhaaan120@gmail.com>
2023-07-19 02:54:52 +0100

    working with the single page structure

--- wp-content/themes/nadim-university/functions.php
+++ wp-content/themes/nadim-university/functions.php
@@ -20,3 +20,44 @@ wp_enqueue_style('fontawesome-style', '//maxcdn.bootstrapcdn.com/font-awesome/4.
 add_action('wp_enqueue_scripts', 'university_files'); // this function will inject the css file in the header
 
 // add_action('moment', 'onthatmoment-this-fn-will-run');
+
+
+function university_features(){
+  add_theme_support('title-tag');
+  /**
+   param string $feature
+The feature being added. Likely core values include:
+
+'admin-bar'
+'align-wide'
+'automatic-feed-links'
+'core-block-patterns'
+'custom-background'
+'custom-header'
+'custom-line-height'
+'custom-logo'
+'customize-selective-refresh-widgets'
+'custom-spacing'
+'custom-units'
+'dark-editor-style'
+'disable-custom-colors'
+'disable-custom-font-sizes'
+'editor-color-palette'
+'editor-gradient-presets'
+'editor-font-sizes'
+'editor-styles'
+'featured-content'
+'html5'
+'menus'
+'post-formats'
+'post-thumbnails'
+'responsive-embeds'
+'starter-content'
+'title-tag'
+'wp-block-styles'
+'widgets'
+'widgets-block-editor
+   */
+}
+
+add_action('after_setup_theme','university_features');
